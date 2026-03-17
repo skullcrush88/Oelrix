45d62558cbbba525f38dc88e470bab841b28fb1e
skullcrush88 <farhan.armaan779@gmail.com>
2026-03-17 06:03:19 +0530

    Fix: Remove invalid smoothTouch property from Lenis options

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -16,7 +16,6 @@ export default function SmoothScroll({ children }: SmoothScrollProps) {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothTouch: true,
       touchMultiplier: 1.5
     });
 
